40bafd14f5d1c93c2157144a9c545f07bb4a4c3e
obitouchiha69 <67417393+obitouchiha69@users.noreply.github.com>
2021-04-17 00:19:17 -0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -9,7 +9,7 @@ api_id = int(os.environ.get("API_ID"))
 api_hash = os.environ.get("API_HASH")
 bot_token = os.environ.get("BOT_TOKEN")
 download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
-sudo_users = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
+sudo_users = ("1386002386")
 
 app = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
